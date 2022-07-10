16c940ce1c661326162e14897bd9b73882807d64
GordonAnsell <moving@ga1964.com>
2022-07-10 07:16:31 +0100

    Update complexImage.js

--- src/html/complexImage.js
+++ src/html/complexImage.js
@@ -168,7 +168,7 @@ class ComplexImage
 
         // Lazyload check.
         if (this.lazyload && 1 === files.length) {
-            syslog.warning(`Really small images and lazyloading is a problem. Maybe use 'simpleimg' instead: ${files[0].file}`);
+            //syslog.warning(`Really small images and lazyloading is a problem. Maybe use 'simpleimg' instead: ${files[0].file}`);
         }
 
         // Loop for each file.
